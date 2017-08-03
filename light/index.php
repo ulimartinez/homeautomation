121251ddda8154de7d1f5cd54f06db0c19c0e679
Ulises <ulimartinez96@gmail.com>
2017-08-03 16:41:55 -0600

    added read gpio

--- light/index.php
+++ light/index.php
@@ -1,8 +1,21 @@
 <?php
+header('Content-Type: application/json');
 $setmode17 = shell_exec("gpio mode 1 out");
 if(isset($_GET['on'])){
-$gpio_on = shell_exec("gpio write 1 1");
+  if(readStatus() != 1)
+    $gpio_on = exec("gpio write 1 1");
+  echo json_encode(new Array('status'=>"on"));
+
 }
 else if(isset($_GET['off'])){
-$gpio_off = shell_exec("gpio write 1 0");
+  if(readStatus() != 0)
+    $gpio_off = exec("gpio write 1 0");
+    echo json_encode(new Array('status'=>"off"));
+}
+else if(isset($_GET['read'])){
+  $state = readStatus();
+  echo json_encode(new Array('status'=>($state == 1? "on" : "off")));
+}
+function readStatus(){
+  return intval(exec("gpio read 1"));
 }
