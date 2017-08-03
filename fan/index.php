d2fba8d7cac71bd6e679eea7f2fbd10cc8dfdbc3
Ulises <ulimartinez96@gmail.com>
2017-08-03 16:48:09 -0600

    test read again

--- fan/index.php
+++ fan/index.php
@@ -1,5 +1,5 @@
 <?php
-header('Content-Type: application/json');
+//header('Content-Type: application/json');
 $setmode17 = shell_exec("gpio mode 4 out");
 if(isset($_GET['on'])){
   if(readStatus() != 1)
@@ -10,7 +10,7 @@ if(isset($_GET['on'])){
 else if(isset($_GET['off'])){
   if(readStatus() != 0)
     $gpio_off = exec("gpio write 4 0");
-    echo json_encode(new Array('status'=>"off"));
+    echo json_encode(array('status'=>"off"));
 }
 else if(isset($_GET['read'])){
   $state = readStatus();
