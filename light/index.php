26633f14a84572c8b4cd6f18af898499877fead9
Ulises <ulimartinez96@gmail.com>
2017-08-03 16:54:17 -0600

    test read again 3

--- light/index.php
+++ light/index.php
@@ -4,17 +4,17 @@ $setmode17 = shell_exec("gpio mode 1 out");
 if(isset($_GET['on'])){
   if(readStatus() != 1)
     $gpio_on = exec("gpio write 1 1");
-  echo json_encode(new Array('status'=>"on"));
+  echo json_encode(array('status'=>"on"));
 
 }
 else if(isset($_GET['off'])){
   if(readStatus() != 0)
     $gpio_off = exec("gpio write 1 0");
-    echo json_encode(new Array('status'=>"off"));
+    echo json_encode(array('status'=>"off"));
 }
 else if(isset($_GET['read'])){
   $state = readStatus();
-  echo json_encode(new Array('status'=>($state == 1? "on" : "off")));
+  echo json_encode(array('status'=>($state == 1? "on" : "off")));
 }
 function readStatus(){
   return intval(exec("gpio read 1"));
